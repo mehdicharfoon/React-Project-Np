1d3ad63abeb96660be554cba3d704d67b983ea27
mehdicharfoon <mehdicharfoon@gmail.com>
2022-04-16 00:07:45 -0400

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -34,6 +34,14 @@ class Main extends Component {
                 />
             );
         };
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };    
 
         return (
             <div>
@@ -41,6 +49,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
